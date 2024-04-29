038efdca0feea50041accba45ec5e3e357c11b2f
Rishi Sharma <RishiPradeep.Sharma@emerson.com>
2024-04-29 12:51:54 +0530

    cors issue

--- dist/app.js
+++ dist/app.js
@@ -8,6 +8,12 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const table_routes_1 = require("./routes/table.routes");
+const corsOptions = {
+    credentials: true,
+    origin: ['http://localhost:3000', 'https://408-ui.netlify.app/'] // Whitelist the domains you want to allow
+};
+
+app.use(cors(corsOptions));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
