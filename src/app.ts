eb1c80b368ebef9badffff73f305ccdb4ac351d7
Rishi Sharma <RishiPradeep.Sharma@emerson.com>
2024-04-29 12:56:46 +0530

    cors issue

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,12 @@ import bodyParser from 'body-parser';
 
 import { router as tableRouter } from './routes/table.routes';
 const app = express();
+const corsOptions = {
+    credentials: true,
+    origin: ['http://localhost:3000', 'https://408-ui.netlify.app/'] // Whitelist the domains you want to allow
+};
+
+app.use(cors(corsOptions));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
