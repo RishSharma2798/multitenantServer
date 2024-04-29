7e9d5e86e52d22ff12ed24fe4165ee9d84219c09
Rishi Sharma <RishiPradeep.Sharma@emerson.com>
2024-04-29 14:57:55 +0530

    cors issue

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,12 @@ import bodyParser from 'body-parser';
 
 import { router as tableRouter } from './routes/table.routes';
 const app = express();
-// const corsOptions = {
-//     credentials: true,
-//     origin: '*' // Allow requests from any origin
-// };
+const corsOptions = {
+    credentials: true,
+    origin: '*' // Allow requests from any origin
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use((req, res, next) => {   res.setHeader('Access-Control-Allow-Origin', '*'); // Replace with your origin  
 res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');   
 res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');   next(); });
